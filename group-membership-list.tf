088a91e92db7e073105bcacc883709f2914fec6c
Guillermo Musumeci <guillermo-musumeci@users.noreply.github.com>
2023-02-26 01:41:30 +0100

    Initial commit

---
+++ group-membership-list.tf
@@ -0,0 +1,14 @@
+##############################################
+## KopiCloud AD API - List Group Membership ##
+##############################################
+
+# List Group Membership for an User
+data "kopicloud_group_membership_list" "test" {
+  user_name  = "guillermo"
+}
+
+# Show Group Membership
+output "OUTPUT_kopicloud_group_membership_list" {
+  description = "Show Group Membership"
+  value       = resource.kopicloud_group_membership.test
+}
